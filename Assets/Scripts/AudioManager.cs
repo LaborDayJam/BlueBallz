e71025377f6077d6b5fa5bade05c6c8c9cb44032
John Li <johnliglobal@gmail.com>
2014-08-30 11:15:01 -0400

    Added AudioManager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+	public AudioClip[] clips;
+
+	//RENAME ME TO ACTUAL SOUNDS
+	public const int SOUND_ONE = 0;
+	public const int SOUND_TWO = 1;
+	public const int LASER_THREE = 2;
+	public const int PLAYER_DEATH = 3;
+	public const int ENEMY_DEATH = 4;
+	public const int POWERUP_PICKUP = 5;
+	
+	private static AudioManager instance;
+	
+	public static AudioManager getInstance()
+	{
+		return instance;	
+	}
+	
+	void Awake()
+	{
+		if(instance != null)
+			Destroy(gameObject);
+		
+		instance = this;
+	}
+	
+	public void Play(int clipIndex)
+	{
+		if(clipIndex < clips.Length)	
+			audio.PlayOneShot(clips[clipIndex], .5f);
+		
+	}
+}
