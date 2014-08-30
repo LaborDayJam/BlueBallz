38f16fb05a477fd6ff6d2dda07f0a5100f04b492
John Li <johnliglobal@gmail.com>
2014-08-30 12:03:31 -0400

    Added Analytics

---
+++ Assets/Plugins/Splyt/InitParams.cs
@@ -0,0 +1,55 @@
+﻿namespace Splyt 
+{
+	/// <summary>
+	/// The InitParams class is a helper class used during initialization of Splyt.
+	/// 
+	/// Use the factory method Splyt.InitParams.create to create an instance of this class.
+	/// </summary>
+	public class InitParams 
+	{
+		internal string customerId { get; private set; }
+		internal EntityInfo deviceInfo { get; private set; }
+		internal EntityInfo userInfo { get; private set; }
+		internal int requestTimeout { get; private set; }
+		internal string host { get; private set; }
+		internal bool logEnabled { get; private set; }
+
+		/// <summary>
+		/// Create a Splyt.InitParams with the given parameters. 
+		/// 
+		/// <b>Note:</b>You should be able to use C# 'named parameters' so that you only need to specify the values that you want to override.
+		/// </summary>
+		/// <param name="customerId">Splyt customer id (contact Splyt if you don't have this)</param>
+		/// <param name="deviceInfo">Initial device info, if available</param>
+		/// <param name="userInfo">Initial user info, if available</param>
+		/// <param name="requestTimeout">The timeout value to use for outgoing http requests</param>
+		/// <param name="host">Host setting (generally reserved for Splyt developers)</param>
+		/// <param name="logEnabled">If set to <c>true</c> logging enabled</param>
+		public static InitParams create(
+			string customerId,
+			EntityInfo deviceInfo = null,
+			EntityInfo userInfo = null,
+			int requestTimeout = Constants.DEFAULT_REQUEST_TIMEOUT,
+			string host = "https://data.splyt.com",
+			bool logEnabled = false) 
+		{
+			InitParams initParams = new InitParams();
+
+			if(null == deviceInfo)
+				deviceInfo = EntityInfo.createDeviceInfo();
+			if(null == userInfo)
+				userInfo = EntityInfo.createUserInfo(null);
+
+			initParams.customerId = customerId;
+			initParams.deviceInfo = deviceInfo;
+			initParams.userInfo = userInfo;
+			initParams.requestTimeout = requestTimeout;
+			initParams.host = host;
+			initParams.logEnabled = logEnabled;
+
+			return initParams;
+		}
+
+		private InitParams() {}
+	}
+}
---
+++ Assets/Scripts/Analytics.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Analytics : MonoBehaviour {
+
+	void Awake()
+	{
+		Splyt.InitParams initParams = Splyt.InitParams.create(
+			"personal17153-test"								// (required) Customer ID from the Splyt team.  If you don't have one, contact them.
+			//,userInfo: Splyt.EntityInfo.createUserInfo("joe")		// (optional) Only necessary if user info is known at startup, otherwise use registerUser later
+			//,logEnabled: true										// (optional) Typically only set to true during development
+			);
+
+		Splyt.Core.init(initParams, delegate(Splyt.Error initError) 
+		                {
+			if(Splyt.Error.Success == initError)
+				Debug.Log("onSplytInitComplete: " + initError.ToString());
+			else
+				Debug.LogError("onSplytInitComplete: " + initError.ToString());
+			
+
+			/*
+			// in this contrived case, we learn about the user just after init - generally this would be triggered by a user action (login?)
+			Splyt.EntityInfo user = Splyt.EntityInfo.createUserInfo(
+				"joe",
+				properties: new Dictionary<string, object> { { "funguy", true }, { "favorite team", "Sweepers" } }
+			);
+			
+			Splyt.Core.registerUser(user, delegate(Splyt.Error registerError) {
+				if(Splyt.Error.Success == registerError)
+					Debug.Log("onSplytRegisterUserComplete: " + registerError.ToString());
+				else
+					Debug.LogError("onSplytRegisterUserComplete: " + registerError.ToString());
+				
+				OnGameReady();
+			});
+			*/
+		});
+
+		Splyt.Instrumentation.Transaction("game").begin();
+		Splyt.Plugins.Session.Transaction().begin();
+		
+	}
+
+	void OnApplicationPause(bool pauseStatus)
+	{
+		if (pauseStatus)
+		{
+			Splyt.Core.pause();
+		}
+		else
+		{
+			Splyt.Core.resume();
+		}
+	}
+
+	//		
+	void OnApplicationQuit()
+	{
+		/*
+		Splyt.Instrumentation.Transaction("game")
+			.setProperties(new Dictionary<string, object> {
+				{ "numberOfMisses", this.mMisses },
+				{ "didWin", won },
+				{ "winQuality", _GetScore() }
+			}).end();
+			*/
+	}
+
+	void PurchaseExample()
+	{
+		//Purchase Example
+		//Splyt.Plugins.Purchase.Transaction(mPurchaseId).end();
+		//Splyt.Plugins.Purchase.Transaction(mPurchaseId).end(Splyt.Constants.TXN_ERROR);
+
+		/*
+		// you don't NEED a purchase id unless multiple purchases may be occuring simulatenously, but it's included here to show the API
+		mPurchaseId = System.Guid.NewGuid().ToString();
+		
+		Splyt.Plugins.Purchase.Transaction(mPurchaseId)
+			.setPointOfSale("Main Menu")
+				.begin();
+		
+		// let's assume we don't learn more about the item until after this purchase process has begun, for this case...  we can add
+		//  more properties through an update
+		Splyt.Plugins.Purchase.Transaction(mPurchaseId)
+			.setPrice(1.99, "usd")
+				.setOfferId("200gold_199")
+				.setItemName("200 Gold")
+				.update(1);
+		
+		mPurchasing = true;
+		*/
+	}
+
+	private void GetVarExample()
+	{
+		//mGameCost = Splyt.Tuning.getVar("newGameCost", 100);
+	}
+}
